fa036402add1de09d26cfdc1fad3bfc045628877
ritesh <ritesh.ramchandani3@gmail.com>
2019-09-24 05:09:29 +0530

    Added new title

--- application/views/welcome_screen.php
+++ application/views/welcome_screen.php
@@ -10,7 +10,7 @@
                      <a class="navbar-brand" href="#">Needs to be done</a>
                 </div>
                 <ul class="nav navbar-nav pull-right">
-                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home Page P</a></li>
                     <li class="dropdown float-right">
                         <a class="dropdown-toggle" data-toggle="dropdown" href="#">Logout<i class="fa fa-user"></i>
                         </a>
