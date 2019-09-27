91b1e964b04bebb42f4233bf93aba344a0241349
Ritesh Ramchandani <52241688+riteshramchandani3@users.noreply.github.com>
2019-09-28 00:18:49 +0530

    changesdone

--- application/views/welcome_screen.php
+++ application/views/welcome_screen.php
@@ -7,7 +7,7 @@
         <nav class="navbar navbar-inverse">
             <div class="container-fluid">
                 <div class="navbar-header">
-                     <a class="navbar-brand" href="#">git learn successfully</a>
+                     <a class="navbar-brand" href="#">git learned</a>
                 </div>
                 <ul class="nav navbar-nav pull-right">
                     <li><a href="#">Home</a></li>
